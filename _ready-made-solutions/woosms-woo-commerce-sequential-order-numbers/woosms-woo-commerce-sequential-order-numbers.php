f1d36746843b808186f944f6d8796f97cb1ebcb6
Lukáš Piják <lukaspijak@gmail.com>
2020-08-07 10:45:01 +0200

    Add files via upload

---
+++ _ready-made-solutions/woosms-woo-commerce-sequential-order-numbers/woosms-woo-commerce-sequential-order-numbers.php
@@ -0,0 +1,52 @@
+<?php
+/*
+  Plugin Name: WooSMS - WooCommerce Sequential Order Numbers
+  Plugin URI: http://www.woo-sms.net/
+  Description: SkyVerge Variables &lt;_order_number&gt;
+  Version: 1.0.0
+  Author: TOPefekt s.r.o. - Lukáš Piják
+  Author URI: http://www.bulkgate.com/
+*/
+
+/**
+ * @author Lukáš Piják 2020 TOPefekt s.r.o.
+ * @link https://www.bulkgate.com/
+ */
+
+use BulkGate\Extensions;
+
+!defined('ABSPATH') && exit;
+
+require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+
+/**
+ * Check if WooSMS is installed
+ */
+if (is_plugin_active('woosms-sms-module-for-woocommerce/woosms-sms-module-for-woocommerce.php'))
+{
+    /**
+     * Extend WooSMS
+     */
+    add_action('woosms_extends_variables', function(Extensions\Hook\Variables $variables, Extensions\Database\IDatabase $database)
+    {
+        if ($variables->get('order_id'))
+        {
+            $data = get_post_meta($variables->get('order_id'));
+
+            if (is_array($data))
+            {
+                foreach ($data as $key => $item)
+                {
+                    if ($key === '_order_number')
+                    {
+                        $variables->set($key, (string) reset($item));
+                    }
+                }
+            }
+        }
+    }, 20, 2);
+}
+else
+{
+    deactivate_plugins(plugin_basename(__FILE__));
+}
